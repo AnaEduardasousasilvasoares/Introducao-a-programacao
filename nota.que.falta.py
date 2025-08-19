420f0777fb8131e7a12300e7a1dea98bbdc369dd
AnaEduardasousasilvasoares <e.sousa@estudante.ifmt.edu.br>
2025-08-19 08:08:35 -0300

    Add files via upload
    atividade 19/08/2025

---
+++ nota.que.falta.py
@@ -0,0 +1,5 @@
+nota1=float(input("Digite a primeira nota: "))
+nota2=float(input("Digite a segunda nota: "))
+notafalta=((60-(nota1*2+nota2*2))/2)
+nota34=notafalta/3
+print("A nota de falta é: ", nota34)
