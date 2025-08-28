6a2170cb0676be0ae54ec1677d2434c3e89e83f2
AnaEduardasousasilvasoares <e.sousa@estudante.ifmt.edu.br>
2025-08-28 10:29:48 -0300

    Add files via upload

---
+++ 4.laço.de.repeticao.py
@@ -0,0 +1,6 @@
+jogadores=int(input("digite o numero de jogadores de volei:"))
+soma_altura=0
+for i in range(jogadores):
+    altura=float(input("digite a altura dos jogadadores:"))
+soma_altura=altura
+print("a media de altura dos jogadores:", soma_altura/jogadores)
