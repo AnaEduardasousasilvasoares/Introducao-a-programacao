602359ed790c945f0e6f3959ca3e95d3052278b8
AnaEduardasousasilvasoares <e.sousa@estudante.ifmt.edu.br>
2025-09-02 08:22:07 -0300

    Add files via upload

---
+++ 9.laço.de.repetição.py
@@ -0,0 +1,10 @@
+n=int(input("digite um numero:"))
+cont=0
+for i in range(1,n):
+    n%2==0
+    cont+1
+if n%i!=0:
+    print("o numero",n,"não é primo")
+else:
+    print("o numero",n,"é primo")
+ 
