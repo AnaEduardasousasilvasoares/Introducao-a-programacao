995b9af41e03556eae0716df32747d3a3e7f79cf
AnaEduardasousasilvasoares <e.sousa@estudante.ifmt.edu.br>
2025-09-04 10:12:02 -0300

    Add files via upload

---
+++ 1.laço.de.repetição.while.py
@@ -0,0 +1,15 @@
+i=1
+somaValor=0
+contNegativo=0
+while i<=20:
+    valor=int(input("digite o valor:"))
+    if valor>=0:
+        somaValor+=valor
+    else: 
+        contNegativo+=1
+    i+=1
+print("A soma dos valores positivos é:",somaValor)
+
+print("A quantidade de valores negativos é:",contNegativo)
+somaValor=0
+contNegativo=0
