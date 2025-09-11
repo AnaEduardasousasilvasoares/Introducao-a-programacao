f2d0b6a83d5a3a722813ecc6516070daf139c720
AnaEduardasousasilvasoares <e.sousa@estudante.ifmt.edu.br>
2025-09-11 10:13:47 -0300

    Add files via upload

---
+++ 7.laço.de.repetição.while.py
@@ -0,0 +1,8 @@
+Thanos=1.50
+Miranha=1.10
+anos=0
+while Miranha<Thanos:
+        Thanos+=0.02
+        Miranha+=0.03
+        anos+=1
+print("vai demorar",anos,"para o homem aranha ser maior que o Thanos")
