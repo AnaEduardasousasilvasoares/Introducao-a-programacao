02116b7ddd9e1e13a8c02d98f93d199b493a8e54
AnaEduardasousasilvasoares <e.sousa@estudante.ifmt.edu.br>
2025-11-04 08:08:19 -0300

    Add files via upload

---
+++ recuperacao.questao5.prova2.py
@@ -0,0 +1,8 @@
+print("Ana Eduarda, Byank, emanuele, Guilherme.")
+n=int(input("digite um numero palíndromo: "))
+s=str(n)
+if s==s[::-1]:
+    print("é polídromo!")
+else:
+    print("não é polídromo!")
+    
